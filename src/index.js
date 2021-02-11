acd4bda364a1a2169303fde41017cd8058692992
frolui <frolui29@gmail.com>
2021-02-11 21:31:41 +0300

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 != 0) {return false}
+    if(str.length == 0) {return true}
+
+    let counter = str.length;
+
+    while (str.length > 0){
+        
+        for (let n = 0; n < bracketsConfig.length; n++){
+            str = str.replace('' + bracketsConfig[n][0] + bracketsConfig[n][1], '')
+        };
+
+        if (str.length == counter) {return false}
+
+        counter = str.length
+    }               
+
+    return true 
 }
